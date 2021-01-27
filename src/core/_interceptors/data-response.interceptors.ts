7e918c15b24a15dba3d5c8f9f6beb07f61461c0a
Abbas Ogaji <abbasogaji@gmail.com>
2021-01-27 18:17:52 +0100

    refactor: default message value

--- src/core/_interceptors/data-response.interceptors.ts
+++ src/core/_interceptors/data-response.interceptors.ts
@@ -7,6 +7,6 @@ export class DataResponseInterceptor implements NestInterceptor {
     context: import('@nestjs/common').ExecutionContext,
     next: import('@nestjs/common').CallHandler<any>,
   ): import('rxjs').Observable<any> | Promise<import('rxjs').Observable<any>> {
-    return next.handle().pipe(map(response => ({ message : response.message || "server response successful" , status: "success", data: response.data || response.data })));
+    return next.handle().pipe(map(response => ({ message : response.message || "API response message" , status: "success", data: response.data || response.data })));
   }
 }
